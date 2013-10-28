0b48cf03ca0db3b11f5084e776f7b607557df4b7
rui.zheng <rui.zheng@tcl.com>
2013-10-28 11:14:24 +0800

    add server

--- client.go
+++ client.go
@@ -2,19 +2,72 @@
 package main
 
 import (
+	"bytes"
 	"log"
+	"net"
 	"net/http"
 )
 
-func handler(w http.ResponseWriter, req *http.Request) {
-	log.Println(req.Body)
+func main() {
+
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	ln, err := net.Listen("tcp", ":8888")
+	if err != nil {
+		// handle error
+		log.Fatal(err)
+	}
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			// handle error
+			log.Println(err)
+			continue
+		}
+		go handleConnection(conn)
+	}
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	log.Printf("About to listen on 8080. Go to https://127.0.0.1:8080/")
-	err := http.ListenAndServeTLS(":8080", "cert.pem", "key.pem", nil)
+func handleConnection(conn net.Conn) {
+	var rbuf, wbuf [1460]byte
+
+	defer conn.Close()
+
+	proxy, err := net.Dial("tcp", ":8118")
 	if err != nil {
 		log.Fatal(err)
+	}
+	defer proxy.Close()
+
+	n, err := conn.Read(rbuf[:])
+	if err != nil {
+		log.Println(err)
+	}
+	log.Printf("read %d data\n", n)
+
+	request, _ := http.NewRequest("POST", "106.187.48.51:8080", bytes.NewBuffer(rbuf[:n]))
+	request.Header.Add("Content-type", "application/octet-stream")
+
+	if n > 0 {
+		var data [4096]byte
+		buf := bytes.NewBuffer(data[:])
+		request.Write(buf)
+		log.Println(string(buf.Bytes()))
+		n, err = proxy.Write(buf.Bytes())
+		if err != nil {
+			log.Fatal(err)
+		}
+		log.Printf("write %d data\n", n)
+	}
+
+	for {
+		n, err := proxy.Read(wbuf[:])
+		if n > 0 {
+			conn.Write(wbuf[:n])
+		}
+		if err != nil {
+			log.Println(err)
+			break
+		}
 	}
 }
---
+++ server.go
@@ -0,0 +1,34 @@
+// server.go
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+	"net/http"
+)
+
+func main() {
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		proxy, err := net.Dial("tcp", ":8118")
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer proxy.Close()
+
+		n, _ := io.Copy(proxy, r.Body)
+		log.Printf("read %d data\n", n)
+		r.Body.Close()
+		if n == 0 {
+			w.Write([]byte("no data"))
+			return
+		}
+		w.Header().Set("Content-type", "application/octet-stream")
+		n, _ = io.Copy(w, proxy)
+		log.Printf("write %d data\n", n)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
