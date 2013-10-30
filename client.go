d2302ba580acdaecd992389bc17d39815c13579f
rui.zheng <rui.zheng@tcl.com>
2013-10-30 14:14:09 +0800

    add https support

--- client.go
+++ client.go
@@ -4,16 +4,40 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"flag"
 	"io"
-	//"io/ioutil"
+	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
+	"strings"
 )
 
-func main() {
+var (
+	proxy  string
+	server string
+)
 
+const (
+	connectURI = "/connect"
+	httpsURI   = "/https"
+	httpURI    = "/http"
+	pollURI    = "/poll"
+)
+
+func init() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
+
+	flag.StringVar(&proxy, "P", "", "http proxy for forward")
+	flag.StringVar(&server, "s", "localhost:8000", "the server that client connecting to")
+	flag.Parse()
+}
+
+func main() {
+	if !strings.HasPrefix(server, "http://") {
+		server = "http://" + server
+	}
+	log.Println(proxy, server)
 
 	ln, err := net.Listen("tcp", ":8888")
 	if err != nil {
@@ -23,7 +47,6 @@ func main() {
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			// handle error
 			log.Println(err)
 			continue
 		}
@@ -31,51 +54,139 @@ func main() {
 	}
 }
 
-func handleConnection(conn net.Conn) {
-	var rbuf, wbuf [1460]byte
-	buf := new(bytes.Buffer)
+func request(method string, urlStr string, body io.Reader, proxy net.Conn) (resp *http.Response, err error) {
+	req, err := http.NewRequest(method, urlStr, body)
 
+	if proxy != nil {
+		if err != nil {
+			return
+		}
+		err = req.WriteProxy(proxy)
+		if err != nil {
+			return
+		}
+		data, err := ioutil.ReadAll(proxy)
+		if err != nil {
+			return nil, err
+		}
+		resp, err = http.ReadResponse(bufio.NewReader(bytes.NewBuffer(data)), req)
+	} else {
+		client := new(http.Client)
+		resp, err = client.Do(req)
+	}
+	return
+}
+
+func handleConnection(conn net.Conn) {
 	defer conn.Close()
 
-	proxy, err := net.Dial("tcp", ":8118")
+	var proxyConn net.Conn
+
+	if len(proxy) > 0 {
+		pConn, err := net.Dial("tcp", proxy)
+		if err != nil {
+			log.Println(err)
+			return
+		}
+		defer pConn.Close()
+		proxyConn = pConn
+	}
+
+	reqData, err := read(conn)
+	if err != nil {
+		log.Println(reqData.Len(), err)
+		return
+	}
+	//log.Println(reqData)
+	r, err := resolve(reqData.Bytes())
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer proxy.Close()
 
-	for {
-		n, err := conn.Read(rbuf[:])
-		//log.Println(n, err)
-		if n > 0 {
-			buf.Write(rbuf[:n])
+	if r.Method == "CONNECT" {
+		resp, err := request("POST", server+connectURI, reqData, proxyConn)
+		if err != nil {
+			log.Println(err)
+			return
 		}
 
-		if n < 1460 || err == io.EOF {
-			break
+		data, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			log.Println(err)
+			return
 		}
+		log.Println(err, resp.StatusCode)
+		if resp.StatusCode != http.StatusOK {
+			return
+		}
+		token := string(data)
+		//log.Println(token)
+		buf := new(bytes.Buffer)
+		buf.WriteString("HTTP/1.0 200 Connection established\r\nProxy-agent: go-http-tunnel\r\n\r\n")
+		conn.Write(buf.Bytes())
+
+		go func() {
+			for {
+				data, err := read(conn)
+				log.Println("read from client", data.Len())
+				if err != nil {
+					log.Println(err)
+					break
+				}
+
+				resp, err := request("POST", server+httpsURI+"?token="+token, data, proxyConn)
+				if err != nil {
+					break
+				}
+				log.Println(err, resp.StatusCode)
+				resp.Body.Close()
+				if resp.StatusCode != http.StatusOK {
+					break
+				}
+			}
+		}()
+
+		for {
+			resp, err := request("GET", server+pollURI+"?token="+token, nil, proxyConn)
+			if err != nil {
+				log.Println(err)
+				break
+			}
+			defer resp.Body.Close()
+
+			log.Println(err, resp.StatusCode)
+			if resp.StatusCode != http.StatusOK {
+				break
+			}
+			_, err = io.Copy(conn, resp.Body)
+			if err != nil {
+				log.Println(err)
+				break
+			}
+		}
+
+		return
 	}
-	//log.Printf("read %d data from request\n", buf.Len())
-	request, _ := http.NewRequest("POST", "http://106.187.48.51:8000/", buf)
-	request.WriteProxy(proxy)
 
-	buf.Reset()
-	for {
-		n, err := proxy.Read(wbuf[:])
-		if n > 0 {
-			buf.Write(wbuf[:n])
-		}
-		//log.Println(n, err)
-		if err == io.EOF {
-			break
-		}
-	}
-	//log.Printf("read %d data from proxy\n", buf.Len())
-
-	resp, err := http.ReadResponse(bufio.NewReader(buf), request)
+	resp, err := request("POST", server+httpURI, reqData, proxyConn)
 	if err != nil {
-		log.Fatal(err)
+		return
 	}
 	defer resp.Body.Close()
-
 	io.Copy(conn, resp.Body)
+}
+
+func read(conn net.Conn) (*bytes.Buffer, error) {
+	var rbuf [1460]byte
+	buf := new(bytes.Buffer)
+
+	n, err := conn.Read(rbuf[:])
+	buf.Write(rbuf[:n])
+	//log.Println("read from connect", buf.Len(), err)
+	return buf, err
+}
+
+func resolve(reqData []byte) (*http.Request, error) {
+	request, err := http.ReadRequest(bufio.NewReader(bytes.NewBuffer(reqData)))
+	return request, err
 }
--- server.go
+++ server.go
@@ -2,32 +2,134 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
 	"io"
 	"log"
+	"math/rand"
 	"net"
 	"net/http"
+	"strconv"
+	"time"
+)
+
+const (
+	BufferSize = 1460
+)
+
+var (
+	conns map[string]net.Conn = make(map[string]net.Conn)
 )
 
 func main() {
 	log.SetFlags(log.LstdFlags | log.Lshortfile)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	http.HandleFunc("/connect", func(w http.ResponseWriter, r *http.Request) {
+		defer r.Body.Close()
+
+		req, err := http.ReadRequest(bufio.NewReader(r.Body))
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		log.Println("https", req.URL.Host)
+		remote, err := net.Dial("tcp", req.URL.Host)
+		if err != nil {
+			log.Println(err)
+			w.WriteHeader(http.StatusServiceUnavailable)
+			return
+		}
+		log.Println("https", req.URL.Host, "done!")
+		rn := rand.New(rand.NewSource(time.Now().UnixNano()))
+		s := strconv.FormatInt(rn.Int63(), 10)
+		conns[s] = remote
+
+		w.Write([]byte(s))
+	})
+
+	http.HandleFunc("/poll", func(w http.ResponseWriter, r *http.Request) {
+		rbuf := make([]byte, 0, BufferSize)
+
+		defer r.Body.Close()
+
+		token := r.FormValue("token")
+		remote, ok := conns[token]
+		if !ok {
+			w.WriteHeader(http.StatusServiceUnavailable)
+			return
+		}
+		log.Println("/poll: start read from remote")
+		n, err := remote.Read(rbuf)
+		log.Println("/poll: read from remote", n)
+		if err != nil {
+			log.Println(n, err)
+			w.WriteHeader(http.StatusServiceUnavailable)
+			delete(conns, token)
+			remote.Close()
+			return
+		}
+
+		//w.Header().Set("Content-Type", "application/octet-stream")
+		w.Write(rbuf)
+	})
+
+	http.HandleFunc("/https", func(w http.ResponseWriter, r *http.Request) {
+		//log.Println(r.RequestURI, r.URL, r.FormValue("token"))
+		defer r.Body.Close()
+
+		token := r.FormValue("token")
+		remote, ok := conns[token]
+		if !ok {
+			log.Println("can't find connect:", token)
+			w.WriteHeader(http.StatusServiceUnavailable)
+			return
+		}
+
+		//log.Println(buf.String())
+		n, err := io.Copy(remote, r.Body)
+		if err != nil {
+			log.Println(n, err)
+			w.WriteHeader(http.StatusServiceUnavailable)
+			delete(conns, token)
+			remote.Close()
+			return
+		}
+		log.Println(n, err)
+	})
+
+	http.HandleFunc("/http", func(w http.ResponseWriter, r *http.Request) {
+		buf := new(bytes.Buffer)
 		proxy, err := net.Dial("tcp", ":8118")
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer proxy.Close()
+		defer r.Body.Close()
 
-		n, _ := io.Copy(proxy, r.Body)
-		r.Body.Close()
-		if n == 0 {
-			w.Write([]byte("no data"))
+		i, err := io.Copy(buf, r.Body)
+		if err != nil {
+			log.Println(i, err)
 			return
 		}
-		w.Header().Set("Content-type", "application/octet-stream")
-		n, _ = io.Copy(w, proxy)
-		//log.Printf("write %d data\n", n)
+
+		//log.Println(buf.String())
+
+		proxy.Write(buf.Bytes())
+
+		//w.Header().Set("Content-Type", "application/octet-stream")
+
+		buf.Reset()
+		io.Copy(buf, proxy)
+		//log.Println(buf)
+		n, err := w.Write(buf.Bytes())
+		if err != nil {
+			log.Println(n, err)
+		}
+		//n, _ := io.Copy(w, proxy)
+		log.Printf("write back %d data\n", n)
 	})
 
+	log.Println("listen on localhost:8000")
 	log.Fatal(http.ListenAndServe(":8000", nil))
 }
